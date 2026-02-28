ddcdcecfa6b0c82d22c699db64b89beee6902eaf
Jorge Fonseca <jorgemjfonseca@users.noreply.github.com>
2026-02-28 10:23:02 +0000

    Release 1.0.0

--- setup.py
+++ setup.py
@@ -2,11 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="pollyweb-domain",
-    version="0.1.1",
+    version="1.0.0",
     description="Domain manifests and message helpers for PollyWeb.",
     author="jorgemf",
     author_email="pollyweb@pollycore.net",
-    license="Apache-2.0",
     url="https://www.pollyweb.org",
     project_urls={
         "Website": "https://www.pollyweb.org",
@@ -15,8 +14,5 @@ setup(
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     include_package_data=True,
-    install_requires=[
-        "pollyweb-utils",
-    ],
     python_requires=">=3.8",
 )
