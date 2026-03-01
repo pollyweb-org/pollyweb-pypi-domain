00599e7303f388a4db67b1de60ae7fb2c58080fa
Jorge Fonseca <jorgemjfonseca@users.noreply.github.com>
2026-03-01 06:38:16 +0000

    Bump package version to 1.0.2 for PyPI publish

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pollyweb-domain",
-    version="1.0.0",
+    version="1.0.2",
     description="Domain manifests and message helpers for PollyWeb.",
     author="jorgemf",
     author_email="pollyweb@pollycore.net",
